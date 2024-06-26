e2aece95603f0038f2895210a9e44e76f1985e57
Yuanxin-Huang <1520214413@qq.com>
2024-06-26 14:50:08 +0800

    RetroSnake made with EasyX

---
+++ RetroSnake/snake.cpp
@@ -0,0 +1,457 @@
+#include <graphics.h>
+#include <conio.h>
+#include<iostream>
+#include<windows.h>
+#include<random>
+#include <time.h>
+#include<vector>
+#include<stdio.h>
+#include<mmsystem.h>
+#pragma comment(lib,"Winmm.lib")
+using namespace std;
+int toDir;
+/*定义宏:f方块宽度、基准坐标x、y，窗口宽度、高度等*/
+
+#define  block_width 22
+#define  x_origin 30
+#define  y_origin 50
+#define x_border 41
+#define y_border 21
+#define windowWidth 1000
+#define windowHeight 700
+#define DIR_UP 1
+#define DIR_DOWN 2
+#define DIR_LEFT 3
+#define DIR_RIGHT 4
+
+/*蛇身体的初始化坐标*/
+int length = 3;
+int block[4000][2];
+/*生成蛇的随机坐标*/
+//void gensnake() {
+//	time_t t1;
+//	time(&t1);
+//	srand((unsigned int)t1);
+//	int x = (rand() % (37 - 1)) + 1;
+//	int y= (rand() % (17 - 1)) + 1;
+//	snake[0][0] = x;
+//	snake[0][1] = y;
+//	snake[1][0] = x++;
+//	snake[1][1] = y;
+//	snake[2][0] = x++;
+//	snake[2][1] = y;
+//}
+int snake[1000][2] = {
+	{9,7},
+	{8,7},
+	{7,7},
+	{0}
+};
+
+/*画出蛇身体的单元格*/
+void drawUnitBlock(int x)
+{
+	int left, top, right, bottom;
+	left = x_origin + block_width * snake[x][0];
+	top= y_origin + block_width * snake[x][1];
+	right = left + block_width;
+	bottom = top + block_width;
+
+	fillrectangle(left, top, right, bottom);
+
+	int left1, top1;
+	left1 = x_origin + block_width * snake[0][0];
+	top1 = y_origin + block_width * snake[0][1];
+
+	IMAGE img1;
+	loadimage(&img1, _T("starhead.jpg"), 25, 25);
+	putimage(left1, top1, &img1);
+
+	IMAGE img2;
+	loadimage(&img2, _T("star.jpg"), 20, 20);
+	putimage(left, top, &img2);
+
+}
+
+/*画出完整的蛇*/
+void drawSnake()
+{
+	setlinestyle(PS_DASH | PS_ENDCAP_ROUND, 1);
+	setlinecolor(WHITE);
+	int i = 0,j=0;
+	for (i = 0; i < length;i++){
+		drawUnitBlock(i);
+	}
+}
+/*绘画背景*/
+	/*每个单元格的坐标*/
+	void drawEveryBlock(int x, int y) 
+	{
+		int left, top, right, bottom;
+		left = x_origin + block_width * block[x][0];
+		top = y_origin + block_width * block[x][1];
+		right = left + block_width;
+		bottom = top + block_width;
+
+		rectangle(left, top, right, bottom);
+	}
+	/*画出单元格*/
+	void drawblock()
+	{
+		setlinestyle(PS_DASH | PS_ENDCAP_ROUND, 1);
+		setlinecolor(WHITE);
+		for (int i = 0; i <40;++i)
+		{
+			for (int j = 0; j <20; ++j)
+			{
+				block[i][0] = i+1;
+				block[i][1] = j+1;
+				drawEveryBlock(i, j);
+			}
+		}
+	}
+	/*画出边框/墙*/
+	void drawframe() 
+	{
+		setlinestyle(PS_SOLID | PS_ENDCAP_ROUND, 6);
+		setlinecolor(YELLOW);
+		rectangle(x_origin+ block_width, y_origin+ block_width,
+			x_origin+ block_width*41, y_origin+ block_width*21);
+	}
+	/*画背景*/
+	void drawbackground() 
+	{
+		cleardevice();
+		IMAGE back;
+		loadimage(&back, _T("background.jpg"), 1020, 800);
+		putimage(0, 0, &back);
+
+		settextstyle(60,0,"楷体");
+		setbkmode(TRANSPARENT);
+		settextcolor(WHITE);
+		outtextxy(300,520,"Your Score");
+
+		drawblock();
+		drawframe();
+	}
+
+	void moveSnake() 
+	{
+		/*读取输入的WASD，并改变蛇的坐标，移动蛇*/
+		char direction = _getch();
+		for (int i = length-1; i > 0; i--) {
+			snake[i][0] = snake[i - 1][0];
+			snake[i][1] = snake[i - 1][1];
+		}
+
+		switch (direction)
+		{
+		case 'w':  
+			if (toDir != DIR_DOWN)  
+			{
+				snake[0][1]--;
+				toDir = DIR_UP;
+				break;
+			}
+		case 's':   
+			if (toDir != DIR_UP)   
+			{
+				snake[0][1]++;
+				toDir = DIR_DOWN;
+				break;
+			}
+		case 'a':    
+			if (toDir != DIR_RIGHT)   
+			{
+				snake[0][0]--;
+				toDir = DIR_LEFT;
+				break;
+			}
+		case 'd':    
+			if (toDir != DIR_LEFT)   
+			{
+				snake[0][0]++;
+				toDir = DIR_RIGHT;
+				break;
+			}
+		default: break;
+		}
+	}
+	/*自动移动*/
+	void autoMove() 
+	{
+		for (int i = length-1; i > 0; i--) 
+		{
+			snake[i][0] = snake[i - 1][0];
+			snake[i][1] = snake[i - 1][1];
+		}
+		switch (toDir) 
+		{
+		case DIR_UP:
+			snake[0][1]--; break;
+		case DIR_DOWN:
+			snake[0][1]++; break;
+		case DIR_LEFT:
+			snake[0][0]--; break;
+		case DIR_RIGHT:
+			snake[0][0]++; break;
+		default: break;
+		}
+		drawSnake();
+	}
+	/*判断蛇是否撞到墙或者遇到自己的身体*/
+	bool checkCollision()
+	{
+
+		/*墙的坐标判断*/
+		if (snake[0][0] >=40 || snake[0][1] >=20 || snake[0][0] <=1 || snake[0][1] <=1) {
+			return false;
+		}
+		/*身体的坐标判断*/
+		for (int i=1;i<length;i++)
+		{
+			if (snake[0][0]== snake[i][0]&& snake[0][1]== snake[i][1])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	int eat = 0;//0表示食物未被吃
+	int food_x;
+	int food_y;
+	/*用随机数生成食物的坐标*/
+	void drawFood(int food_x,int food_y) 
+	{
+		setfillcolor(YELLOW);
+		fillrectangle(food_x * block_width + x_origin, food_y * block_width + y_origin,
+			(food_x + 1) * block_width + x_origin, (food_y + 1) * block_width + y_origin);
+	}
+	void generateFood() 
+	{
+		time_t t;
+		time(&t);
+		srand((unsigned int)t);
+		food_x = (rand() % (40-2))+2;//生成范围在2~40  (rand()%(b-a))+a
+		food_y = (rand() % (20 - 2)) + 2;
+		int flag = 0;//判断标志，为1时画出食物
+		/*生成食物坐标不与蛇的坐标重合*/
+		for (int i = 0; i<length; i++)
+		{
+			if (food_x != snake[i][0] && food_y != snake[i][1]) 
+			{
+				flag = 1;
+			}
+		}
+		if (flag) 
+		{
+			drawFood(food_x, food_y);
+		}
+
+	}/*判断食物是否被蛇吃，用蛇头坐标和食物坐标重合为标准*/
+	
+	int eatFood()
+	{
+		eat = 0;
+		if (snake[0][0] == food_x && snake[0][1] == food_y) {
+			eat = 1;//表示食物被吃
+		}
+		return eat;
+	}
+	/*增长蛇的长度*/
+	void increaseLength()
+	{
+		int tailX, tailY;
+		if (eatFood() == 1) {
+			tailX = snake[length - 1][0];
+			tailY = snake[length - 1][1];
+			autoMove();
+			snake[length][0] = tailX;
+			snake[length][1] = tailY;
+			length++;
+			drawSnake();
+		}
+	}
+	/*判断食物是否被吃*/
+	int score = 0;
+	void checkFood() {
+
+		
+		TCHAR s[5];
+
+		if (eatFood() == 1)
+		{
+			increaseLength();
+			generateFood();
+			drawbackground();
+			drawSnake();
+			drawFood(food_x, food_y);
+			score++;
+			_stprintf_s(s, _T("%d"), score);
+			outtextxy(400, 580, s);
+
+		}
+		else
+		{
+			drawFood(food_x, food_y);
+			drawSnake();
+			_stprintf_s(s, _T("%d"), score);
+			outtextxy(400, 580, s);
+		}
+	}
+	/*游戏失败界面的设置*/
+	void gameover() 
+	{
+		mciSendString(_T("close ./bgm2.mp3 "), NULL, NULL, NULL);
+		mciSendString(_T("open ./loser.mp3 alias bgm"), NULL, NULL, NULL);
+		mciSendString(_T("play ./loser.mp3 repeat"), NULL, NULL, NULL);
+
+		initgraph(600, 800);
+		SetWindowText(GetHWnd(), "error");
+		IMAGE img5;
+		loadimage(&img5, "lose.jpg", 600, 800);
+		putimage(0, 0, &img5);
+
+		setbkmode(TRANSPARENT);
+		settextcolor(WHITE);
+		settextstyle(50, 0, "楷体");
+		outtextxy(80, 60, "啊啊啊啊啊啊！失败了！");
+		outtextxy(70, 110, "不要气馁！");
+		outtextxy(70, 160, "我们再来一次！");
+		setbkmode(TRANSPARENT);
+		settextcolor(RED);
+		
+		setbkmode(TRANSPARENT);
+		settextcolor(BLACK);
+		settextstyle(20, 0, "楷体");
+		outtextxy(70, 600, "作者的小私心，我又回坑闪耀暖暖了，我的女鹅太好看了叭！");
+		outtextxy(70, 625, "我好爱！都来看看我的美腻女鹅！");
+		outtextxy(70, 650, "没办法，太爱了！");
+
+	}
+	void begin() {
+		/*开始窗口*/
+		initgraph(600, 800);
+		SetWindowText(GetHWnd(), "暖暖");
+		mciSendString(_T("open ./bgm.mp3 alias bgm"), NULL, NULL, NULL);
+		mciSendString(_T("play ./bgm.mp3 repeat"), NULL, NULL, NULL);
+		
+
+		IMAGE img3;
+		loadimage(&img3, "start.jpg", 600, 800);
+		putimage(0, 0, &img3);
+
+		setbkmode(TRANSPARENT);
+		settextcolor(WHITE);
+		settextstyle(50, 0, "楷体");
+		outtextxy(190, 650, "暖暖之中二贪吃蛇");
+		_getch();
+		closegraph();
+
+		/*规则说明窗口*/
+		initgraph(600, 800);
+		IMAGE img4;
+		loadimage(&img4, "start2.jpg", 600, 800);
+		putimage(0, 0, &img4);
+
+		setbkmode(TRANSPARENT);
+		settextcolor(WHITE);
+		settextstyle(50, 0, "楷体");
+		outtextxy(160, 60, "规则说明");
+		settextstyle(30, 0, "楷体");
+		outtextxy(60, 110, "  苏暖暖在拯救奇迹大陆的旅途中");
+		outtextxy(50, 140, "    失去了力量，无法战斗");
+		outtextxy(60, 170, "     她需要你的帮助");
+		outtextxy(80, 570, "我一起拯救奇迹大陆！");
+		setbkmode(TRANSPARENT);
+		settextcolor(RED);
+		settextstyle(30, 0, "楷体");
+		outtextxy(60, 400, "操纵键盘上的 W A S D");
+		outtextxy(60, 430, "W:上 A:左 S:下 D：右");
+		outtextxy(60, 460, "注意：1、不要碰到身体");
+		outtextxy(60, 490, "     2、不要触碰黄色边框");
+		setbkmode(TRANSPARENT);
+		settextcolor(RED);
+		settextstyle(40, 0, "楷体");
+		outtextxy(60, 520, "尽可能多得获取黄色能量块");
+		
+
+		_getch();
+		mciSendString(_T("close ./bgm.mp3 "), NULL, NULL, NULL);
+		closegraph();
+		/*cleardevice();
+		while (1) {
+			int x = 100, y = 200;
+			char k = _getch();
+			if (k == 'w') {
+				y -= 25;
+			}
+			else if (k == 's') {
+				y += 25;
+			}
+			else if (k == 'a') {
+				x -= 25;
+			}
+			else if (k == 'd') {
+				x += 25;
+			}
+			else if (k == ' ') {
+				break;
+			}
+		}*/
+	}
+		int main()
+		{
+			begin();
+			mciSendString(_T("open ./bgm2.mp3 "), NULL, NULL, NULL);
+			mciSendString(_T("play ./bgm2.mp3 repeat"), NULL, NULL, NULL);
+			/*游戏界面窗口*/
+			initgraph(windowWidth, windowHeight);
+			cleardevice();
+			drawbackground();
+
+			drawSnake();
+			toDir = 4;
+			char input = _getch();
+			generateFood();
+			drawFood(food_x, food_y);
+			
+			while (1) 
+			{
+				BeginBatchDraw();
+					if (_kbhit())
+					{
+							moveSnake();
+							drawbackground();
+							checkFood();
+							if (checkCollision() == 0) {
+								gameover();
+								_getch();
+								mciSendString(_T("close ./loser.mp3 "), NULL, NULL, NULL);
+								closegraph();
+								break;
+							}
+					}
+					else
+					{
+							autoMove();
+							drawbackground();
+							drawSnake();
+							checkFood();
+							Sleep(200);
+							if (checkCollision() == 0) {
+								gameover();
+								_getch();
+								mciSendString(_T("close ./loser.mp3 "), NULL, NULL, NULL);
+								closegraph();
+								break;
+							}
+					}
+					FlushBatchDraw();
+			}
+			EndBatchDraw();
+			_getch();
+			closegraph();
+			return 0;
+		}
